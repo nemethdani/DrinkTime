1fd251e6651008bb89dc8ae49e2f3d56a1739393
Daniel Nemeth <nemethdanka@gmail.com>
2020-12-16 23:10:23 +0100

    Hydration event törölve

--- app/src/main/java/hu/bme/aut/android/drinktime/model/HydrationEvent.kt
+++
@@ -1,9 +0,0 @@
-package hu.bme.aut.android.drinktime.model
-
-import java.time.LocalDateTime
-
-class HydrationEvent(val drink: DrinkType, val volumeMl:Int) {
-    var localDateTime=LocalDateTime.now()
-
-}
-
