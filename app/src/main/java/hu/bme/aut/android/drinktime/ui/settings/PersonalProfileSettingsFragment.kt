135a7e2492888e91793a77a9947b7c80c829ffe3
Daniel Nemeth <nemethdanka@gmail.com>
2020-12-16 21:27:08 +0100

    personal profile preferences summaries, testbutton ok

--- app/src/main/java/hu/bme/aut/android/drinktime/ui/settings/PersonalProfileSettingsFragment.kt
+++ app/src/main/java/hu/bme/aut/android/drinktime/ui/settings/PersonalProfileSettingsFragment.kt
@@ -1,7 +1,10 @@
 package hu.bme.aut.android.drinktime.ui.settings
 
+import android.R.attr
 import android.content.SharedPreferences
 import android.os.Bundle
+import androidx.preference.EditTextPreference
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.PreferenceManager
 import hu.bme.aut.android.drinktime.DrinkTimeApplication.Companion.Person
@@ -13,6 +16,7 @@ class PersonalProfileSettingsFragment : PreferenceFragmentCompat(),
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.personal_profile_preferences, rootKey)
+
     }
 
     override fun onStart() {
@@ -30,6 +34,20 @@ class PersonalProfileSettingsFragment : PreferenceFragmentCompat(),
 
 
     override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
+
+
+        val cs :CharSequence =key ?:""
+        val pref : Preference? = findPreference(cs)
+
+        //Update the summary with user input data
+        if (pref is EditTextPreference){
+            //Get the user input data
+            val prefixStr = sharedPreferences!!.getString(key, "")
+
+            pref?.setSummary(prefixStr)
+        }
+
+
         Person.setupPerson(this.context)
 
     }
